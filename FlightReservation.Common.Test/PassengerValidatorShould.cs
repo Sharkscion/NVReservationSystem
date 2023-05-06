de6a2e61f745c21d39c11f0946498b8960b9e6ee
stan <shayane_tan@yahoo.com>
2023-05-06 19:26:56 +0800

    Created Unit Tests for common utility methods of the project

---
+++ FlightReservation.Common.Test/PassengerValidatorShould.cs
@@ -0,0 +1,38 @@
+﻿using FlightReservation.Common.Validators;
+
+namespace FlightReservation.Common.Test
+{
+    public class PassengerValidatorShould
+    {
+        public static IEnumerable<object[]> GetBirthDates()
+        {
+            yield return new object[] { DateTime.Now, false };
+            yield return new object[] { DateTime.Now.AddDays(-15), false };
+            yield return new object[] { DateTime.Now.AddDays(-16), true };
+            yield return new object[] { DateTime.Now.AddYears(-18), true };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetBirthDates))]
+        public void ValidateBirthDate(DateTime value, bool expectedResult)
+        {
+            bool result = PassengerValidator.IsBirthDateValid(value);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData(" ", false)]
+        [InlineData("Name Space", false)]
+        [InlineData("Name1", false)]
+        [InlineData("Name", true)]
+        [InlineData("abcdefghijklmnopqrst", true)]
+        [InlineData("abcdefghijklmnopqrstu", false)]
+        public void ValidateName(string value, bool expectedResult)
+        {
+            bool result = PassengerValidator.IsNameValid(value);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ FlightReservation.Common.Test/ReservationValidatorShould.cs
@@ -0,0 +1,36 @@
+﻿using FlightReservation.Common.Validators;
+
+namespace FlightReservation.Common.Test
+{
+    public class ReservationValidatorShould
+    {
+        public static IEnumerable<object[]> GetFlightDates()
+        {
+            yield return new object[] { DateTime.Now.AddDays(-1), false };
+            yield return new object[] { DateTime.Now, true };
+            yield return new object[] { DateTime.Now.AddDays(1), true };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFlightDates))]
+        public void ValidateFlightDate(DateTime value, bool expectedResult)
+        {
+            bool result = ReservationValidator.IsFlightDateValid(value);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("ABC", false)]
+        [InlineData("abcdef", false)]
+        [InlineData("1ABCDE", false)]
+        [InlineData("ABCDEFG", false)]
+        [InlineData("ABCDEF", true)]
+        [InlineData("A2CDE3", true)]
+        public void ValidateBookingReference(string value, bool expectedResult)
+        {
+            bool result = ReservationValidator.IsBookingReferenceFormatValid(value);
+            Assert.Equal(expectedResult, result);
+        } 
+    }
+}
