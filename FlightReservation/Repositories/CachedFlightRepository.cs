9746f90c7c99e38f6d2035fabff7d6dde015634b
stan <shayane_tan@yahoo.com>
2023-05-07 16:41:30 +0800

    Created unit test for CachedFlightRepository and cleaned-up code

--- FlightReservation/Repositories/CachedFlightRepository.cs
+++ FlightReservation/Repositories/CachedFlightRepository.cs
@@ -12,51 +12,6 @@ namespace FlightReservation.Repositories
             _flights = new HashSet<IFlight>();
         }
 
-        /*
-                public void Save(IFlight flight)
-                {
-                    _flights.Add(flight);
-                }
-        
-                public IEnumerable<IFlight> FindAllHaving(int flightNumber)
-                {
-                    return _flights.Where((flight) => flight.FlightNumber == flightNumber);
-                }
-        
-                public IEnumerable<IFlight> FindAllHaving(string airlineCode)
-                {
-                    return _flights.Where((flight) => flight.AirlineCode == airlineCode);
-                }
-        
-                public IEnumerable<IFlight> FindAllHaving(string airlineCode, int flightNumber)
-                {
-                    return _flights.Where(
-                        (flight) => flight.AirlineCode == airlineCode && flight.FlightNumber == flightNumber
-                    );
-                }
-        
-                public IEnumerable<IFlight> FindAllHaving(string origin, string destination)
-                {
-                    return _flights
-                        .Where((flight) => flight.DepartureStation == origin)
-                        .Where((flight) => flight.ArrivalStation == destination);
-                }
-        
-                public IFlight? Find(
-                    int flightNumber,
-                    string airlineCode,
-                    string origin,
-                    string destination
-                )
-                {
-                    return _flights
-                        .Where((flight) => flight.FlightNumber == flightNumber)
-                        .Where((flight) => flight.AirlineCode == airlineCode)
-                        .Where((flight) => flight.DepartureStation == origin)
-                        .Where((flight) => flight.ArrivalStation == destination)
-                        .FirstOrDefault();
-                }
-        */
         public IQueryable<IFlight> List()
         {
             return _flights.AsQueryable();
