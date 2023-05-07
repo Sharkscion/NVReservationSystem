b8420a695fabd7a7b837fa652f16df48abfea4dc
stan <shayane_tan@yahoo.com>
2023-05-07 20:14:27 +0800

    Create unit tests for both create and view methods of FlightService

---
+++ FlightReservation.Test/Services/CreateFlightServiceShould.cs
@@ -0,0 +1,80 @@
+﻿using FlightReservation.Models.Flight;
+using FlightReservation.Test.Services.Fixtures;
+
+namespace FlightReservation.Test.Services
+{
+    public class CreateFlightServiceShould : IClassFixture<FlightServiceFixture>
+    {
+        private readonly FlightServiceFixture _fixture;
+
+        public CreateFlightServiceShould(FlightServiceFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Create_AFlight()
+        {
+            var model = new FlightModel
+            {
+                AirlineCode = "NEW",
+                FlightNumber = 1,
+                DepartureStation = "MNL",
+                ArrivalStation = "CEB",
+            };
+
+            bool successful = _fixture.Service.Create(model);
+
+            Assert.True(successful);
+        }
+
+        [Fact]
+        public void ReturnTrue_IfFlightExists()
+        {
+            var model = new FlightModel
+            {
+                AirlineCode = "NV",
+                FlightNumber = 1,
+                DepartureStation = "MNL",
+                ArrivalStation = "CEB",
+            };
+
+            bool exists = _fixture.Service.DoesExists(model);
+
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public void ReturnFalse_IfFlightDoesNotExists()
+        {
+            var model = new FlightModel
+            {
+                AirlineCode = "NO",
+                FlightNumber = 2,
+                DepartureStation = "MNL",
+                ArrivalStation = "CEB",
+            };
+
+            bool exists = _fixture.Service.DoesExists(model);
+
+            Assert.False(exists);
+        }
+
+
+        [Fact]
+        public void RaiseError_OnDuplicateFlight()
+        {
+            var existingFlight = new FlightModel
+            {
+                AirlineCode = "NV",
+                FlightNumber = 1,
+                DepartureStation = "MNL",
+                ArrivalStation = "CEB",
+            };
+
+            Action action = () => _fixture.Service.Create(existingFlight);
+
+            Assert.Throws<DuplicateFlightException>(action);
+        }
+    }
+}
---
+++ FlightReservation.Test/Services/Fixtures/FakeFlightRepository.cs
@@ -0,0 +1,80 @@
+﻿using FlightReservation.Models.Contracts;
+using FlightReservation.Models.Flight;
+using FlightReservation.Repositories.Contracts;
+using System.Text;
+
+namespace FlightReservation.Test.Services.Fixtures
+{
+    public class FakeRepository : IFlightRepository, IDisposable
+    {
+        private readonly HashSet<IFlight> _flights;
+
+        public FakeRepository()
+        {
+            _flights = new HashSet<IFlight>
+            {
+                new FlightModel
+                {
+                    AirlineCode = "NV",
+                    FlightNumber = 1,
+                    DepartureStation = "MNL",
+                    ArrivalStation = "CEB",
+                    DepartureScheduledTime = new TimeOnly(hour: 8, minute: 0),
+                    ArrivalScheduledTime = new TimeOnly(hour: 9, minute: 0)
+                },
+                new FlightModel
+                {
+                    AirlineCode = "NV",
+                    FlightNumber = 1,
+                    DepartureStation = "MNL",
+                    ArrivalStation = "CEB",
+                    DepartureScheduledTime = new TimeOnly(hour: 14, minute: 0),
+                    ArrivalScheduledTime = new TimeOnly(hour: 15, minute: 0)
+                },
+                new FlightModel
+                {
+                    AirlineCode = "NV",
+                    FlightNumber = 1,
+                    DepartureStation = "MNL",
+                    ArrivalStation = "DVO",
+                    DepartureScheduledTime = new TimeOnly(hour: 20, minute: 0),
+                    ArrivalScheduledTime = new TimeOnly(hour: 21, minute: 30)
+                },
+                new FlightModel
+                {
+                    AirlineCode = "MK",
+                    FlightNumber = 2,
+                    DepartureStation = "MNL",
+                    ArrivalStation = "SIN",
+                    DepartureScheduledTime = new TimeOnly(hour: 20, minute: 0),
+                    ArrivalScheduledTime = new TimeOnly(hour: 22, minute: 0)
+                },
+                new FlightModel
+                {
+                    AirlineCode = "MK",
+                    FlightNumber = 3,
+                    DepartureStation = "SIN",
+                    ArrivalStation = "MNL",
+                    DepartureScheduledTime = new TimeOnly(hour: 20, minute: 0),
+                    ArrivalScheduledTime = new TimeOnly(hour: 22, minute: 0)
+                },
+            };
+        }
+
+        public void Dispose()
+        {
+            _flights.Clear();
+        }
+
+        public IQueryable<IFlight> List()
+        {
+            return _flights.AsQueryable();
+        }
+
+        public bool Save(IFlight item)
+        {
+            _flights.Append(item);
+            return true;
+        }
+    }
+}
