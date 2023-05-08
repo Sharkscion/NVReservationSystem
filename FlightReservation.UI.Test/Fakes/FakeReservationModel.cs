bb06883938315863c2c53977cbe570359e12843c
stan <shayane_tan@yahoo.com>
2023-05-08 13:52:56 +0800

    Created unit tests for SearchReservationPresenter

---
+++ FlightReservation.UI.Test/Fakes/FakeReservationModel.cs
@@ -0,0 +1,24 @@
+﻿using FlightReservation.Models.Contracts;
+
+namespace FlightReservation.UI.Test.Fakes
+{
+    internal class FakeReservationModel : IReservation
+    {
+        public IFlight FlightInfo { get; set; }
+        public DateTime FlightDate { get; set; }
+
+        public string PNR { get; }
+
+        public IEnumerable<IPassenger> Passengers { get; set; }
+
+        public FakeReservationModel(string bookingReference)
+        {
+            PNR = bookingReference;
+        }
+
+        public IReservation FromBookingReference(string bookingReference)
+        {
+            return new FakeReservationModel(bookingReference);
+        }
+    }
+}
